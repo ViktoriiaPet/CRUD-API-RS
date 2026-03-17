7d6117ca64bdd9604b6202c52b1d0ad0d0c42827
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-03-17 17:52:33 +0100

    fix: update port reading from dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import Fastify from "fastify";
 import { getUserProducts } from "./controller.js";
 import { items } from "./servise.js";
 import { getAllProducts } from "./controller.js";
+import dotenv from "dotenv";
 
+dotenv.config();
+const PORT = Number(process.env.PORT);
 
+if (isNaN(PORT)) {
+  throw new Error("PORT is not defined or invalid in .env");
+}
 const fastify = Fastify({
   logger: true
 })
@@ -12,7 +18,7 @@ fastify.get('/api/products/', getAllProducts);
 
 fastify.get<{Params: { userId: string }}> ('/api/products/:userId', getUserProducts)
 try {
-  await fastify.listen({ port: 4000 })
+  await fastify.listen({ port: PORT })
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
